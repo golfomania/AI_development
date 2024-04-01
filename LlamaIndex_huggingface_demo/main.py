43fdf247a5c4048de5717dffd0cb36e6e9f05b67
golfomania <ml.loeffler@gmx.de>
2024-04-01 22:54:48 +0200

    persistant version, too less ram

--- LlamaIndex_huggingface_demo/main.py
+++ LlamaIndex_huggingface_demo/main.py
@@ -2,11 +2,16 @@
 
 # pip install llama-index-core llama-index-readers-file llama-index-llms-ollama llama-index-embeddings-huggingface
 
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+import os.path
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+    Settings
+)
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.llms.ollama import Ollama
-
-documents = SimpleDirectoryReader("data").load_data()
 
 # bge embedding model
 Settings.embed_model = resolve_embed_model("local:BAAI/bge-m3")
@@ -14,12 +19,22 @@ Settings.embed_model = resolve_embed_model("local:BAAI/bge-m3")
 # ollama
 Settings.llm = Ollama(model="gemma", request_timeout=60.0)
 
-index = VectorStoreIndex.from_documents(
-    documents,
-)
+# check if storage already exists
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
 
 # query engine
 query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
-# response = query_engine.query("Was hat der Autor in seiner Jugend gemacht?")
+# response = query_engine.query("What did the author do growing up?")
 print(response)
